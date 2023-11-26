7767d2c791112539788b7862bb23e22292b39147
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2023-11-26 13:45:44 +0900

    [変更]セッション情報のDMS対応

--- lib/net/types/impl/niconicoSessionInfo.ts
+++ lib/net/types/impl/niconicoSessionInfo.ts
@@ -1,8 +1,8 @@
 import { NiconicoSessionInfo } from "../../../../@types/net/hooks/types/niconicoSessionInfo";
 
 export class NiconicoSessionInfoImpl implements NiconicoSessionInfo {
-    DmcResponseJsonData = "";
-    ContentUrl = "";
-    SessionId = "";
-    KeyURI = "";
+  DmcResponseJsonData = "";
+  ContentUrl = "";
+  SessionId = "";
+  IsDMS = false;
 }
